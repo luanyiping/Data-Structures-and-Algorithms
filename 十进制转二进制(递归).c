51ed614c4dfc95ca7876603bbd8c38fc5d9c48d6
luanyiping <3349244951@qq.com>
2021-01-12 14:54:53 +0800

    十进制转二进制(递归)

---
+++ 十进制转二进制(递归).c
@@ -0,0 +1,57 @@
+//#include<stdio.h>
+//void to2(int num)
+//{
+//	if (num == 0)
+//		return;
+//	else
+//	{
+//		to2(num / 2);
+//		printf("%d", num % 2);
+//	}
+//}
+//int main()
+//{
+//	int num;
+//	scanf("%d", &num);
+//	to2(num);
+//
+//}
+//#include<stdio.h>
+//#define N 100
+//struct Stack
+//{
+//	int data[N];
+//	int top; // 标识栈顶
+//};
+//typedef struct Stack stack; // struct Stack的别名
+//// 初始化栈
+//void init(stack* p)
+//{
+//	p->top = -1;
+//
+//}
+//// 判断栈是否为空
+//int isempty(stack* p)
+//{
+//
+//}
+//// 判断栈是否满
+//int isfull(stack* p)
+//{
+//
+//}
+//// 获取栈顶数据
+//int gettop(stack* p)
+//{
+//
+//}
+//// 入栈
+//void push(stack* p, int key)
+//{
+//
+//}
+//// 出栈
+//void pop(stack* p)
+//{
+//
+//}
